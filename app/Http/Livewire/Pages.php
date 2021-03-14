474da44ba20f014cfffaab3d3394d78da3ab0167
Cristhian Coaquira <cristhiandanielcoaquira@gmail.com>
2021-03-14 14:38:46 -0400

    Install laravel-trix and implement page form

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -6,6 +6,27 @@ use Livewire\Component;
 
 class Pages extends Component
 {
+    public $title;
+    public $slug;
+    public $content;
+
+    public $modalFormVisible = false;
+    
+    /**
+     * Shows the modal form
+     *
+     * @return void
+     */
+    public function createShowModal()
+    {
+        $this->modalFormVisible = true;
+    }
+
+    public function create()
+    {
+        $this->reset();
+    }
+
     public function render()
     {
         return view('livewire.pages');
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,3 +1,54 @@
-<div>
-    {{-- Stop trying to control. --}}
+<div class="p-6">
+    <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6">
+        <x-jet-button wire:click='createShowModal'>
+            {{ __('Create') }}
+        </x-jet-button>
+    </div>
+    {{-- Modal Form --}}
+    <x-jet-dialog-modal wire:model="modalFormVisible">
+        <x-slot name="title">
+            {{ __('Save Page') }}
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="mt-4">
+                <x-jet-label for="title" value="{{ __('Title') }}" />
+                <x-jet-input id="title" class="block w-full mt-1" type="text" required
+                    wire:model.debounce.800ms='title' />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="slug" value="{{ __('Slug') }}" />
+                <div class="flex mt-1 rounded-md shadow-sm">
+                    <span
+                        class="inline-flex items-center px-3 text-sm text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50">
+                        http://localhost:8000/
+                    </span>
+                    <input wire:model="slug"
+                        class="flex-1 block w-full transition duration-150 ease-in-out rounded-none form-input rounded-r-md sm:text-sm sm:leading-5"
+                        placeholder="url-slug">
+                </div>
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="content" value="{{ __('Content') }}" />
+                <div class="rounded-md shadow-sm">
+                    <div class="mt-1 bg-white">
+                        <div class="body-content" wire:ignore>
+                            <trix-editor class="trix-content" x-ref="trix" wire:model.debounce.100000ms="content" wire:key='trix-content-unique-key'>
+                            </trix-editor>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('modalFormVisible')" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                {{ __('Delete Account') }}
+            </x-jet-danger-button>
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
