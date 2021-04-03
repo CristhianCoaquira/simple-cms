faaa7cefc687a89c8af39ebf72962ad0bbb76d26
Cristhian Coaquira <cristhiandanielcoaquira@gmail.com>
2021-04-02 20:34:18 -0400

    Implement Menus in FrontPage

--- app/Http/Livewire/Frontpage.php
+++ app/Http/Livewire/Frontpage.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\NavigationMenu;
 use App\Models\Page;
 use Livewire\Component;
 
@@ -30,8 +31,27 @@ class Frontpage extends Component
         $this->content = $data->content;
     }
 
+    private function sidebarLinks()
+    {
+        return NavigationMenu::where('type', 'SidebarNav')
+            ->orderBy('sequence', 'asc')
+            ->orderBy('created_at', 'asc')
+            ->get();
+    }
+
+    private function topNavLinks()
+    {
+        return NavigationMenu::where('type', 'TopNav')
+            ->orderBy('sequence', 'asc')
+            ->orderBy('created_at', 'asc')
+            ->get();
+    }
+
     public function render()
     {
-        return view('livewire.frontpage')->layout('layouts.frontend');
+        return view('livewire.frontpage', [
+            'sidebarLinks' => $this->sidebarLinks(),
+            'topNavLinks' => $this->topNavLinks(),
+        ])->layout('layouts.frontend');
     }
 }
